77cd7a46f39fbaff9cbf2c2620b31bc33598cbba
Timo <tjmaul@gmail.com>
2016-02-10 10:59:10 +0100

    Initial Template Commit

---
+++ Awesome/Awesome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Awesome
+//
+//  Created by Timo Maul on 10.02.16.
+//  Copyright © 2016 Timo Maul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
